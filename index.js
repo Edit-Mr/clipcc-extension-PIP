7791a878f63fdcbe3540bb4c0b1dc47a3a43f87c
Edit Mr <elvismao.070512@gmail.com>
2022-12-22 13:15:07 +0800

    finished

--- index.js
+++ index.js
@@ -1,36 +1,75 @@
 const { Extension, type, api } = require('clipcc-extension');
-
+//yarn build
 class MyExtension extends Extension {
     onInit() {
-        const stage = getStageCanvas();
         api.addCategory({
             categoryId: 'em.pip.category',
             messageId: 'em.pip.category',
-            color: '#66CCFF'
+            color: '#FFA500'
         });
         api.addBlock({
             opcode: 'em.pip.status',
             type: type.BlockType.REPORTER,
             messageId: 'em.pip.status',
             categoryId: 'em.pip.category',
-            function: () => 'Hello, ClipCC!'
+            function: () => this.stat()
+        });
+        api.addBlock({
+            opcode: 'em.pip.avaliable',
+            type: type.BlockType.COMMAND,
+            messageId: 'em.pip.avaliable',
+            categoryId: 'em.pip.category',
+            function: args => this.avaliable()
+        });
+        api.addBlock({
+            opcode: 'em.pip.start',
+            type: type.BlockType.COMMAND,
+            messageId: 'em.pip.start',
+            categoryId: 'em.pip.category',
+            function: args => this.start()
         });
         api.addBlock({
             opcode: 'em.pip.on',
             type: type.BlockType.COMMAND,
             messageId: 'em.pip.on',
             categoryId: 'em.pip.category',
-            function: args => this.HelloWorld()
+            function: args => this.enablePictureInPicture(video)
+        });
+        api.addBlock({
+            opcode: 'em.pip.off',
+            type: type.BlockType.COMMAND,
+            messageId: 'em.pip.off',
+            categoryId: 'em.pip.category',
+            function: args => this.closePipWindow()
         });
     }
     onUninit() {
         api.removeCategory('em.pip.category');
     }
+    stat() {
+        if (document.pictureInPictureElement == video) return true; return false;
+    }
+    avaliable() {
+        if (document.pictureInPictureEnabled) return true; return false;
+    }
     start() {
-        document.body.innerHTML += '<video id="video" width="300" height="150" autoplay muted></video>';
-        const canvas = getStageCanvas();
-        const video = document.getElementById('video');
+        console.log("starting");
+        const newElement = document.createElement('div');
 
+        // Set the element's content
+        newElement.innerHTML = '<video id="video" width="480" height="360" autoplay muted style="display:none;"></video>';
+        // Add the element to the top of the body
+        document.body.prepend(newElement);
+        const video = document.getElementById('video');
+        const canvas = document.getElementsByTagName('canvas')[0];
+
+        // Check if the element was found
+        if (canvas) {
+            // The element was found, do something with it
+        } else {
+            // The element was not found
+            console.error('Could not find canvas element');
+        }
         // Stream the canvas to the video element
         let stream;
         if (canvas.captureStream) {
@@ -56,6 +95,44 @@ class MyExtension extends Extension {
         } else {
             console.error('Picture-in-Picture is not supported');
         }
+    }
+    closePipWindow() {
+        // Check if the current document is in a PIP window
+        if (document.pictureInPictureElement) {
+            // Exit Picture-in-Picture mode
+            document.exitPictureInPicture().catch((error) => {
+                console.error(error);
+            });
+        }
+    }
+
+    captureCanvas(canvas) {
+        // Create a new MediaStream with a single video track
+        const stream = new MediaStream();
+
+        // Create a frame-by-frame update function
+        function update() {
+            // Get the current image data from the canvas
+            const imageData = canvas.toDataURL('image/webp');
+
+            // Create an image element from the image data
+            const img = new Image();
+            img.src = imageData;
+
+            // Draw the image onto the canvas
+            canvas.getContext('2d').drawImage(img, 0, 0);
+
+            // Add the frame to the stream
+            stream.addTrack(canvas.captureStream().getVideoTracks()[0]);
+
+            // Schedule the next frame update
+            requestAnimationFrame(update);
+        }
+
+        // Start updating the stream
+        update();
+
+        return stream;
     }
 }
 
